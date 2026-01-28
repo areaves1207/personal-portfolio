474602dce0bb37f607d2ddf62df61bb09b3959bd
areaves1207 <areaves0816@gmail.com>
2026-01-29 00:47:12 +0100

    Removed vue boilerplate

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,16 @@ import Intro from "./components/Intro.vue"
 </script>
 
 <template>
-  <main>
+  <main class="wrapper">
     <Intro></Intro>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
+  .wrapper{
+    width: 100%; height: 100%;
+  }
+/* @media (min-width: 1024px) {
   
-}
+} */
 </style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,31 @@
 
 <template>
-    <div class="intro">
-        <h1>Hello!</h1>
+    <div class="wrapper">
+        <h1>Hello! I'm Aidan Reaves, a graduate from Colorado School of Mines with both my Masters and Bachelors in computer science!</h1>
+        <h4>Sub text here</h4>
+
+        <button>Check out my portfolio!</button>
     </div>
 </template>
+
+<style scoped>
+    .wrapper{
+        width: 100%;
+        height: 100%;
+        background-color: red;
+    }
+
+    h1{
+
+    }
+
+    h4{
+
+    }
+
+    button{
+
+    }
+
+</style>
 
