a6a2b373980835a28877a2fc9dce4872c1fc7e26
areaves1207 <areaves0816@gmail.com>
2026-01-29 02:04:35 +0100

    Added background and updated title text

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,9 +2,9 @@
 <template>
     <div class="wrapper">
         <h1>Hello! I'm Aidan Reaves, a graduate from Colorado School of Mines with both my Masters and Bachelors in computer science!</h1>
-        <h4>Sub text here</h4>
+        <h4>This is my simple portfolio!</h4>
 
-        <button>Check out my portfolio!</button>
+        <button>Check it out</button>
     </div>
 </template>
 
@@ -12,31 +12,52 @@
     .wrapper{
         width: 100%;
         height: 100%;
-        background-color: red;
+        background-color: rgba(110, 136, 193);
         margin: 0 auto;
+        padding: 3% 15% 0 15%;
+        padding-bottom: 25%;
         text-align: center;
 
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+
+        background-image: url("../assets/images/sorrento_dusk_blurred.jpg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%; 
+        background-position: bottom; 
     }
 
     h1{
-
+        color: white;
+        font-size: xx-large;
+        padding-bottom: 25px;
     }
 
     h4{
+        font-size: larger;
+        color: rgb(227, 227, 227);
     }
 
     button{
-        background-color: rgb(1, 126, 126);
+
+        background-color: rgb(84, 104, 146);
         border: none;
         border-radius: 8px;
-        padding: 16px;
+        padding: 25px;
+        margin-top: 80px;
         color: white;
+
+        font-size: x-large;
+
+        width: 30%;
         
         transition: background-color 0.5s ease, color 0.5s ease; 
     }
 
     button:hover{
-        background-color: rgb(0, 161, 161);
+        background-color: rgba(110, 136, 193);
         cursor: pointer;
     }
 
